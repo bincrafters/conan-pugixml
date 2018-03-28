ff3e8d24834c3391a7e4abc87a06775cf08de4b3
Uilian Ries <uilianries@gmail.com>
2018-03-28 18:23:17 -0300

    Improve test package
    - Add real case for package test
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -11,23 +11,13 @@ class pugixmlConan(ConanFile):
     description = "Light-weight, simple and fast XML parser for C++ with XPath support"
     url = "https://github.com/bincrafters/conan-pugixml"
     homepage = "https://github.com/zeux/pugixml"
-
-    # Indicates License type of the packaged library
     license = "MIT"
-
-    # Packages the license for the conanfile.py
     exports = ["LICENSE.md"]
-
-    # Remove following lines if the target lib does not use cmake.
     exports_sources = ["CMakeLists.txt"]
     generators = "cmake"
-
-    # Options may need to change depending on the packaged library.
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = "shared=False", "fPIC=True"
-
-    # Custom attributes for Bincrafters recipe conventions
     source_subfolder = "source_subfolder"
     build_subfolder = "build_subfolder"
 
@@ -37,18 +27,16 @@ class pugixmlConan(ConanFile):
             del self.options.fPIC
 
     def source(self):
-        source_url = "https://github.com/zeux/pugixml"
+        source_url = self.homepage
         tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
         extracted_dir = self.name + "-" + self.version
-
-        #Rename to "source_subfolder" is a convention to simplify later steps
         os.rename(extracted_dir, self.source_subfolder)
 
     def configure_cmake(self):
         cmake = CMake(self)
         # pugixml use lib64 on linux/x86_64
         cmake.definitions["CMAKE_INSTALL_LIBDIR"] = "lib"
-        cmake.definitions["BUILD_TESTS"] = False # example
+        cmake.definitions["BUILD_TESTS"] = False
         if self.settings.os != 'Windows':
             cmake.definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
         cmake.configure(build_folder=self.build_subfolder)

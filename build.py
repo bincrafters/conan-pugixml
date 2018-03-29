16dd96ca799130b0912e6fbb2c25c15e630c559e
Uilian Ries <uilianries@gmail.com>
2018-03-28 21:01:48 -0300

    Fix header only build
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -4,12 +4,12 @@
 
 import os
 from bincrafters import build_template_default
-from bincrafters import build_template_header_only
 
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder()
     if os.getenv("CONAN_HEADER_ONLY", False):
-        builder = build_template_header_only.get_builder()
+        builder.builds = []
+        builder.add(settings={}, options={"pugixml:header_only": True}, env_vars={}, build_requires={})
 
     builder.run()

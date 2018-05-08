579d3ca241ae2622b391495ef12a63eadb482998
Uilian Ries <uilianries@gmail.com>
2018-05-08 20:31:58 -0300

    #265 Remove fPIC from build stage
    - Conan 1.3.0 set fPIC automatically
    - Run YAPF to format code according PEP8
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -44,9 +44,7 @@ class pugixmlConan(ConanFile):
         # pugixml use lib64 on linux/x86_64
         cmake.definitions["CMAKE_INSTALL_LIBDIR"] = "lib"
         cmake.definitions["BUILD_TESTS"] = False
-        if self.settings.os != 'Windows':
-            cmake.definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
-        elif self.settings.os == 'Windows' and self.settings.compiler == 'Visual Studio':
+        if self.settings.os == 'Windows' and self.settings.compiler == 'Visual Studio':
             cmake.definitions['CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS'] = self.options.shared
         cmake.configure(build_folder=self.build_subfolder)
         return cmake
